c11cedff4d767eb4a6ce2779468a039a987b3d7a
wellington.siqueira <wellingtonex@gmail.com>
2016-12-08 22:08:04 -0300

    passando parametros para o componente da maneira nova

--- client/app/foto/foto.component.ts
+++ client/app/foto/foto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class FotoComponent {
     
+    @Input() url;
+    @Input() titulo;
+
 }
