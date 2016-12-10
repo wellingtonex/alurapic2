957c87a14a888b350d4fee763cca8d5fa2bbc6a2
wellington.siqueira <wellingtonex@gmail.com>
2016-12-10 17:13:09 -0300

    criando PainelComponent, que por sua vez tera um poutro componente com conteudo.

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { FotoModule } from './foto/foto.module';
 import { HttpModule } from '@angular/http';
+import { PainelModule } from './painel/painel.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FotoModule, HttpModule ],
+  imports:      [ BrowserModule, FotoModule, HttpModule, PainelModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
---
+++ client/app/painel/painel.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'painel',
+    templateUrl: './painel.component.html'
+})
+export class PainelComponent {
+
+    @Input() titulo: string;
+
+    constructor() {
+
+    }
+}
